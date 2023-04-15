220ff8d2ab8fc7694086aa61695ddc324a70daf9
panupong mikada <path301242@gmail.com>
2023-04-15 13:54:18 +0700

    Schema

--- apidemo/apidemo/urls.py
+++ apidemo/apidemo/urls.py
@@ -23,6 +23,35 @@ api = NinjaAPI()
 class HelloSchema(Schema):
     name: str = "World"
 
+# Can't use
+# class UserSchema(Schema):
+#     username: str
+#     is_authenticated: bool
+#     email: str = None
+#     first_name: str = None
+#     last_name: str = None
+
+# @api.get("/me", response=UserSchema)
+# def me(request):
+#     return request.user
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool = True#False
+    email: str
+    first_name: str
+    last_name: str
+
+class Error(Schema):
+    message: str
+
+@api.get("/me", response={200: UserSchema, 403: Error})
+def me(request):
+    user = UserSchema(username="pmikada", email="test@example.com", first_name="Panupong", last_name="Mikada")
+    if not user.is_authenticated:
+        return 403, {"message": "Please sign in first"}
+    return user
+
 #/api/hello?name=you => "Hello you"
 # @api.get("/hello")
 @api.post("/hello")
